40c9e310bb29631721d0b0ee78c8e2c2298bb8f1
toxar2033 <72166711+toxar2033@users.noreply.github.com>
2023-04-12 19:56:33 +0300

    updated

--- main.cpp
+++ main.cpp
@@ -4,35 +4,31 @@
 
 using namespace std;
 
+template<typename t>
 class user_it{
 private:
-  vector<int> :: iterator a;
-  vector<int> :: iterator b;
-  vector<int> :: iterator curr;
-  bool check_a = true;
-  bool check_b = false;
+  typename t :: iterator a;
+  typename t :: iterator b;
+  typename t :: iterator curr;
 public:
-  user_it(vector<int> :: iterator tmp1, vector<int> :: iterator tmp2){
+  user_it(typename t :: iterator tmp1, typename t :: iterator tmp2){
     a = tmp1; b = --tmp2; curr = tmp1;
-    if(a == b){
-      check_b = true;
-    }
   };
 
   void operator++ (){if (curr != b)++curr;
     else cout<<"Error: end reached\n";};
   void operator-- (){if (curr != a)--curr;
     else cout<<"Error: begining reached\n";};
-  int operator* (){ return *curr;};
-  vector<int> :: iterator cur(){ return curr; };
+  typename t :: value_type operator* (){ return *curr;};
+  typename t :: iterator cur(){ return curr; };
 };
 
 int main(){
-  vector <int> task = {1,2,3,4,5};
-  vector <int> :: iterator it_A = task.begin();
-  vector <int> :: iterator it_B = task.end();
-  user_it example = user_it(it_A, it_B);
-  cout << *example.cur() << endl;
+  vector <char> task = {'3','1','2','4','5'};
+  vector <char> :: iterator it_A = task.begin();
+  vector <char> :: iterator it_B = task.end();
+  user_it <vector<char>> example{it_A, it_B};
+ cout << *example << endl;
   --example;
   cout << *example.cur() << endl;
   ++example;
