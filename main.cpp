27198fdf24f983b9eb93f8369685d88a43e229a6
BigSnowHill <semiklit.katya@gmail.com>
2023-04-13 16:05:15 +0300

    operators

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <iterator>
 #include <vector>
-#include <any>
 
 /** фактически описанный почтовый итератор должен представлять собой работу функции zip
  * zip должна уметь складывать в один объект несколько объектов разного типа из данных ей контейнеров
@@ -17,19 +16,75 @@ private:
     std::vector<ContainerIterator> others;
     Functor* function;
 public:
-    ZipIter(ContainerIterator firstIter, ContainerIterator others[], Functor* function){};
-    typename ContainerIterator::value_type operator*(){};
-    typename ContainerIterator::value_type operator[] (int n){};
-    ZipIter operator+ (){};
-    ZipIter operator- (){};
-    ZipIter operator++ (){};
-    ZipIter operator-- (){};
-    bool operator== (ZipIter* secondIter){};
-    bool operator!= (ZipIter* secondIter){};
+    ZipIter(ContainerIterator firstIter, ContainerIterator others[], Functor* function):
+            current(firstIter), function(function) {
+        for(auto i: others){
+            this->others.push_back(i);
+        }
+    }
+
+    typename ContainerIterator::value_type operator*(){
+        return function(this->current, this->others);
+    };
+
+    ZipIter operator++ (){
+        current++;
+        for(int i = 0; i < others.size(); ++i){
+            others[i]++;
+        }
+        return *this;
+    };
+
+    ZipIter operator-- (){
+        current--;
+        for(int i = 0; i < others.size(); ++i){
+            others[i]--;
+        }
+        return *this;
+    };
+
+    ZipIter operator+ (int n){
+        for(int i = 0; i < n; ++i){
+            (*this)++;
+        }
+        return *this;
+    };
+
+    ZipIter operator- (int n){
+        for(int i = 0; i < n; ++i){
+            (*this)++;
+        }
+        return *this;
+    };
+
+    bool operator== (ZipIter* secondIter){
+        if(this->current != secondIter->current){
+            return false;
+        }
+        for(int i = 0; i < secondIter->others.size(); ++i){
+            if(others[i] != secondIter->others[i]){
+                return false;
+            }
+        }
+        return true;
+    };
+
+    bool operator!= (ZipIter* secondIter){
+        if(this->current == secondIter->current){
+            return false;
+        }
+        for(int i = 0; i < secondIter->others.size(); ++i){
+            if(others[i] == secondIter->others[i]){
+                return false;
+            }
+        }
+        return true;
+    };
 };
 
 
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello world!";
     return 0;
 }
